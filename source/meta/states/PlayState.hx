25819cbaa861f2329d1070b3848d6b145498709b
StarNovaBR <creambr6@gmail.com>
2026-01-08 13:14:38 -0300

    Ok

--- source/meta/states/PlayState.hx
+++ source/meta/states/PlayState.hx
@@ -4988,7 +4988,7 @@ class PlayState extends MusicBeatState
 		var controlHoldArray:Array<Bool> = [left, down, up, right,dodge];
 
 		// TO DO: Find a better way to handle controller inputs, this should work for now
-		if(ClientPrefs.controllerMode)
+		if(!ClientPrefs.controllerMode)
 		{
 			var controlArray:Array<Bool> = [controls.NOTE_LEFT_P, controls.NOTE_DOWN_P, controls.NOTE_UP_P, controls.NOTE_RIGHT_P];
 			var mobileArray:Array<Bool> = [mobileManager.hitbox.buttonLeft.justPressed, mobileManager.hitbox.buttonDown.justPressed, mobileManager.hitbox.buttonUp.justPressed, mobileManager.hitbox.buttonRight.justPressed];
@@ -5033,7 +5033,7 @@ class PlayState extends MusicBeatState
 		}
 
 		// TO DO: Find a better way to handle controller inputs, this should work for now
-		if(ClientPrefs.controllerMode)
+		if(!ClientPrefs.controllerMode)
 		{
 			var controlArray:Array<Bool> = [controls.NOTE_LEFT_R, controls.NOTE_DOWN_R, controls.NOTE_UP_R, controls.NOTE_RIGHT_R];
 			var mobileArray:Array<Bool> = [mobileManager.hitbox.buttonLeft.justReleased, mobileManager.hitbox.buttonDown.justReleased, mobileManager.hitbox.buttonUp.justReleased, mobileManager.hitbox.buttonRight.justReleased];
